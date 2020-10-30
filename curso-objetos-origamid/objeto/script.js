413dab675e4e53b10ca118ee78cc35cdd861e776
thiagonmiziara <thiagonmiziara@gmail.com>
2020-10-30 19:26:04 -0300

    finalizando mais uma aula

--- curso-objetos-origamid/objeto/script.js
+++ curso-objetos-origamid/objeto/script.js
@@ -76,3 +76,73 @@ Object.defineProperties(moto2, {
         this._velocidade = 'velocidade ' + valor;
     },
 });
+
+////////////////OBJECT.GETOWNPROPERTYDESCRIPTORS(OBJ)////////
+
+Object.getOwnPropertyDescriptors(Array);
+// Lista com métodos e propriedades e Array
+
+Object.getOwnPropertyDescriptors(Array.prototype);
+// Lista com métodos e propriedades do protótipo de Array
+
+Object.getOwnPropertyDescriptor(window, 'innerHeight');
+// Puxa de uma única propriedade
+
+
+////////////OBJECT.KEYS(OBJ), OBJECT.VALUES(OBJ) OBJECT.ENTRIES(OBJ)///
+
+Object.keys(Array);
+//[] vazia, pois não possui propriedades enumeráveis
+
+const carro3 = {
+    marca: 'Ford',
+    ano: 2018,
+}
+Object.keys(carro3);
+// ['marca', 'ano']
+Object.values(carro3);
+// ['Ford', 2018]
+Object.entries(carro3);
+// [['marca', 'Ford'], ['ano', 2018]]
+
+
+
+/////////OBJECT.GETOWNPROPERTYNAMES(OBJ)//////////
+
+Object.getOwnPropertyNames(Array);
+// ['length', 'name', 'prototype', 'isArray', 'from', 'of']
+
+Object.getOwnPropertyNames(Array.prototype);
+// [..., 'filter', 'map', 'every', 'some', 'reduce', ...]
+
+const carro4 = {
+    marca: 'Ford',
+    ano: 2018,
+}
+Object.getOwnPropertyNames(carro4);
+// ['marca', 'ano']
+
+/////////OBJECT.GETPROTOTYPEOF() E OBJECT.IS()///////
+
+const frutas = ['Banana', 'Pêra']
+Object.getPrototypeOf(frutas);
+Object.getPrototypeOf(''); // String
+
+const frutas1 = ['Banana', 'Pêra'];
+const frutas2 = ['Banana', 'Pêra'];
+
+Object.is(frutas1, frutas2); // false
+
+///OBJECT.FREEZE(), OBJECT.SEAL(), OBJECT.PREVENTEXTENSIONS()///
+
+const carro5 = {
+    marca: 'Ford',
+    ano: 2018,
+}
+Object.freeze(carro5);
+Object.seal(carro5);
+Object.preventExtensions(carro5);
+
+Object.isFrozen(carro5); // true
+Object.isSealed(carro5); // true
+Object.isExtensible(carro5); // FALSE
