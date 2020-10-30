06e1e1926c8db8fd04073c1beba4a81fdc8caf9f
thiagonmiziara <thiagonmiziara@gmail.com>
2020-10-30 19:38:02 -0300

    finalizando mais uma aula

--- curso-objetos-origamid/objeto/script.js
+++ curso-objetos-origamid/objeto/script.js
@@ -146,3 +146,51 @@ Object.preventExtensions(carro5);
 Object.isFrozen(carro5); // true
 Object.isSealed(carro5); // true
 Object.isExtensible(carro5); // FALSE
+
+
+////PROPRIEDADES E MÉTODOS DO PROTÓTIPO////
+
+const frutas7 = ['Banana', 'Uva'];
+frutas7.constructor; // Array
+
+const frase = 'Isso é uma String';
+frase.constructor; // String
+
+///{}.HASOWNPROPERTY('PROP') E {}.PROPERTYISENUMERABLE('PROP')///
+
+const frutas3 = ['Banana', 'Uva'];
+
+frutas3.hasOwnProperty('map'); // false
+Array.hasOwnProperty('map'); // false
+Array.prototype.hasOwnProperty('map'); // true
+
+Array.prototype.propertyIsEnumerable('map'); // false
+window.propertyIsEnumerable('innerHeight'); // true
+
+
+///{}.ISPROTOTYPEOF(VALOR)////
+
+const frutas4 = ['Banana', 'Uva'];
+
+Array.prototype.isPrototypeOf(frutas4); // true
+
+/////{}.TOSTRING() ////
+
+const frutas5 = ['Banana', 'Uva'];
+frutas5.toString(); // 'Banana,Uva'
+typeof frutas5; // object
+Object.prototype.toString.call(frutas5); // [object Array]
+
+const frase1 = 'Uma String';
+frase1.toString(); // 'Uma String'
+typeof frase1; // string
+Object.prototype.toString.call(frase1); // [object String]
+
+const carro6 = { marca: 'Ford' };
+carro6.toString(); // [object Object]
+typeof carro6; // object
+Object.prototype.toString.call(carro6); // [object Object]
+
+const li6 = document.querySelectorAll('li');
+typeof li6; // object
+Object.prototype.toString.call(li6); // [object NodeList]
