52246d4162a2f87f4b5a3589a5768b628e8d53eb
thiagonmiziara <thiagonmiziara@gmail.com>
2020-10-30 17:43:53 -0300

    finalizando mais uma aula

--- curso-objetos-origamid/function/script.js
+++ curso-objetos-origamid/function/script.js
@@ -76,3 +76,63 @@ const filtro = Array.prototype.filter.call(li, function(item) {
 });
 
 console.log(filtro); // retorna os item que possuiem ativo
+
+
+////////////////////////////////////////function.apply()
+
+const numeros = [3, 4, 6, 134, 44, 32];
+
+Math.max.apply(null, numeros);
+Math.max.call(null, 3, 4, 5, 6, 7, 20);
+
+// Podemos passar null para o valor
+// de this, caso a função não utilize
+// o objeto principal para funcionar
+
+const li1 = document.querySelectorAll('li');
+
+function itemPossuiAtivo(item) {
+    return item.classList.contains('ativo');
+}
+
+const filtro1 = Array.prototype.filter.call(li, itemPossuiAtivo);
+const filtro2 = Array.prototype.filter.apply(li, [itemPossuiAtivo]);
+
+//// functtion.bind()
+
+const li2 = document.querySelectorAll('li');
+
+const filtrarLi = Array.prototype.filter.bind(li, function(item) {
+    return item.classList.contains('ativo');
+})
+
+console.log(filtrarLi()); // [li.ativo]
+
+
+const carro = {
+    marca: 'Ford',
+    ano: 2018,
+    acelerar: function(aceleracao, tempo) {
+        return `${this.marca} acelerou ${aceleracao}Km em ${tempo} segundos`;
+    }
+}
+
+carro.acelerar(100, 20); // ""Ford acelerou 100Km em 20 segundos"
+
+const honda = {
+    marca: 'Honda'
+}
+
+const acelerarHonda = carro.acelerar.bind(honda);
+acelerarHonda(200, 10); //"Honda acelerou 200Km em 10 segundos"
+
+
+/////argumentos comuns
+
+function imc(altura, peso) {
+    return peso / (altura * altura);
+}
+
+const imc180 = imc.bind(null, 1.80);
+imc(1.80, 70); //21.6
+imc180(70); //21.6
